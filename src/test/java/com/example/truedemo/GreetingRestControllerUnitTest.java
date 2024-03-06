30620739733fb72f4aec84d9927f2b3d4b9818de
Derrick Wong <derrick.chwong@gmail.com>
2024-03-06 16:16:16 +0700

    polish

--- src/test/java/com/example/truedemo/GreetingRestControllerUnitTest.java
+++ src/test/java/com/example/truedemo/GreetingRestControllerUnitTest.java
@@ -1,12 +1,10 @@
 package com.example.truedemo;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
